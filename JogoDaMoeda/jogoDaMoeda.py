605e4ed030e086c170043d6f7c2ef8db10f5eb82
nicoolasaraujo <nicolas.araujo@ufu.br>
2018-12-08 14:58:00 -0200

    Implementando montyHall e jogo da moeda

--- JogoDaMoeda/jogoDaMoeda.py
+++ JogoDaMoeda/jogoDaMoeda.py
@@ -1,5 +1,8 @@
 import random as R
 # import threading from threading
+NE=25
+ORCAMENTO = 0
+
 def umaJogadaMoedaViciada(L=3):
   s = 0
   gasto = 0
@@ -28,41 +31,59 @@ def umaJogada(L=3):
   return 8-gasto
 
 def umaJogadaCapInicial(capInicial,L=3):
-  print('Capital Inicial: ' + str(capInicial))
   s = 0
   gasto = 0
-  while ((not abs(s) == L) & (capInicial[0] > 0) ): #continua jogando até que a diferença seja maior que L e possua capInicial[0]
+  while ((not abs(s) == L)): #continua jogando até que a diferença seja maior que L e possua capInicial[0]
     r = R.randint(0, 1)
     if r == 0:
       s -= 1
     elif r == 1:
       s += 1
     gasto += 1
-    capInicial[0] -=1
+    if capInicial > 0: #quando o capital tiver acabado o seu valor não deve mais ser drecementado, porque o "crédito" será contabilizado no gasto
+      capInicial -=1
+  return 8-gasto + capInicial # o resultado final será o quanto ele ganhou + o capital inicial
 
-  if (not abs(s) == L):
-    return gasto * (-1)
-
-  return 8-gasto
-
+def umaJogadaOrcamento(orcamento,L=2):
+  # print(orcamento)
+  s = 0
+  gasto = 0
+  while ((not abs(s) == L) and (orcamento > 0)): #continua jogando até que a diferença seja maior que L e possua capInicial[0]
+    r = R.randint(0, 1)
+    if r == 0:
+      s -= 1
+    elif r == 1:
+      s += 1
+    gasto += 1
+    orcamento -=1
+  if(abs(s) == L):
+    return 0
+  return 1
+  
 def main():
-  NE=1000
-  '''diferença de 3'''
-  print('Vai executar diferença de 3')
-  exp = [umaJogada() for _ in range(NE)]
-  print(str(sum(exp)/(NE*1.0)))  
+  # '''diferença de 3'''
+  # print('Vai executar diferença de 3')
+  # exp = [umaJogada() for _ in range(NE)]
+  # print(str(sum(exp)/(NE*1.0)))  
 
-  '''diferença de 2'''
-  print('Vai executar diferença de 2')
-  exp = [umaJogada(2) for _ in range(NE)]
-  print(str(sum(exp)/(NE*1.0)))  
+  # '''diferença de 2'''
+  # print('Vai executar diferença de 2')
+  # exp = [umaJogada(2) for _ in range(NE)]
+  # print(str(sum(exp)/(NE*1.0)))  
 
-  '''Diferença 3 e capInicial'''
-  print('Diferença 3 e capInicial')
-  capInicial = 5 
-  # print(capInicial[0])
-  exp = [umaJogadaCapInicial(capInicial) for _ in range(NE)]
-  print(str(sum(exp)/(NE*1.0)))  
+  # '''Diferença 3 e capInicial'''
+  # print('Diferença 3 e capInicial')
+  # for capInicial in range(3, 9): 
+  #   capInicial = int(capInicial) 
+  #   print('Capital Inicial: {}'.format(capInicial))
+  #   exp = [umaJogadaCapInicial(capInicial) for _ in range(NE)]
+  #   print(str(sum(exp)/(NE*1.0)))  
+  for i in range(1,26):
+    print('Orçamento: {}'.format(i), end="\t")
+    # print('Orçamento: {}'.format(orcamento))
+    exp = [umaJogadaOrcamento(i) for _ in range(NE)]
+    print(str(25 * (sum(exp)/(NE*1.0))))
+
 
 if __name__ =="__main__":
   main()
--- Monty Hall/montyHall.py
+++ Monty Hall/montyHall.py
@@ -1,30 +1,43 @@
 import random 
 
-def populaPortas(portas):
-    indiceCarro =random.randint(0,2)
+NE = 10
+
+def selecionaCarro(portas):
+    indiceCarro = random.randint(0,2)
     portas[indiceCarro] = 1    
     return indiceCarro
 
 def umaJogada(portas, portaEscolhida, trocaPorta=None):
-  print('Portas: {}'.format(portas))
-  print('Porta Escolhida: {}'.format(portaEscolhida))
   if trocaPorta:
-    return portas[portaEscolhida] == 1
-  return portas[portaEscolhida] == 1
+    if portas[portaEscolhida] == 0: #se trocou de porta, a porta escolhida anteriormente deverá ser igual a zero
+      return 1
+  elif portas[portaEscolhida] == 1: #se ele não trocou de porta, ele ganha se a porta escolhida for 1
+    return 1
+  return 0
+
 
 def main():
-    portas = [0,0,0]
-    portaCarro = populaPortas(portas)
+  rodadaSemTroca = []
+  rodadaComTroca = []
+
+  for _ in range(NE):
+    portas = {0:0, 1:0, 2:0}
+    portaCarro = selecionaCarro(portas)
     portaRemovida = 0
     portaEscolhida = random.randint(0,2)
+
     while True:
       portaRemovida = random.randint(0,2)
       if portaRemovida != portaCarro and portaRemovida != portaEscolhida :
         portas.pop(portaRemovida)
-        if portaRemovida
         break
-    print('Resultado: {}'.format(umaJogada(portas, portaEscolhida)))
+
+    rodadaSemTroca.append(umaJogada(portas, portaEscolhida))
+    rodadaComTroca.append(umaJogada(portas, portaEscolhida, trocaPorta=True))
+  
+  print('Não trocou de porta: {}'.format(sum(rodadaSemTroca)/(NE*1.0)))
+  print('Trocou de porta: {}'.format(sum(rodadaComTroca)/(NE*1.0)))
+
 
 if __name__ =="__main__":
   main()
-
