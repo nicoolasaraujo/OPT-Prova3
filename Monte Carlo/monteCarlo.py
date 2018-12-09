dc296791d2c6fa4d546067c9a74f942c62123a55
nicoolasaraujo <nicolas.araujo@ufu.br>
2018-12-09 19:03:46 -0200

    Implementação random walk e medida de tempo

--- Monte Carlo/monteCarlo.py
+++ Monte Carlo/monteCarlo.py
@@ -1,24 +1,64 @@
 import random
 import math
+import time
 
-NE = 10000000
+NE = 10000
+RESTRICAOX = 99953
+RESTRICAOY = 100015
+
 
 def defineCoordenadas():
-    y = random.randrange(0, 100050)
-    x = random.randrange(0, 100050)
-    return x,y
+  y = random.randrange(0, RESTRICAOY)
+  x = random.randrange(0, RESTRICAOX)
+  return x,y
 
-def executaMonteCarlo():
+def executaMonteCarlo(x=None, y=None):
+  if not x and not y:
     x,y = defineCoordenadas()
-    z = math.sin(x/50000) + math.cos(y/30000)
-    return z
+  z = math.sin(x/50000) + math.cos(y/30000)
+  return z
+
+
+def RandomWalk():
+  x,y = defineCoordenadas()
+  z = executaMonteCarlo()
+  while True:
+    direcao = random.choice(['NORTE', 'SUL', 'LESTE','OESTE'])
+    if direcao == 'NORTE':
+      y += random.randint(0, (RESTRICAOY - y))
+    elif direcao == 'SUL':
+      y -= random.randint(0,  y)
+    elif direcao == 'LESTE':
+      x += random.randint(0, RESTRICAOX - x)
+    elif direcao == 'OESTE':
+      x -= random.randint(0,  x)
+
+    novoZ = executaMonteCarlo(x,y)
+    if z >= novoZ:
+      break
+    else:
+      z = novoZ
+
+  return z
 
 def main():
-    '''Execução Monte Carlo Simples'''
-    exp = [executaMonteCarlo() for _ in range(NE)]
-    print(str(max(exp)))
 
+  print('Execução Monte Carlo Simples')
+  tempoInicio = time.time()  
+  '''Execução Monte Carlo Simples'''
+  exp = [executaMonteCarlo() for _ in range(NE)]
+  print(str(max(exp)))
+  print('Tempo despendido {}'.format(time.time() - tempoInicio))
+  print('---------------------------------------')
 
+  print('Execução Monte random walk')
+  tempoInicio = time.time()
+  '''Execução Monte random walk'''
+  exp = [RandomWalk() for _ in range(NE)]
+  print(str(max(exp)))
+  print('Tempo despendido {}'.format(time.time() - tempoInicio))
+  print('---------------------------------------')
+  
 
 if __name__ =="__main__":
   main()
